15fda25895a1f47e1e1a415dc84410fcb04d4522
Vinitpanchal12 <vinitjpanchal2002@gmail.com>
2023-12-20 11:29:33 +0530

    Oauth with google strategy done

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require('express')
+const express = require('express');
+require('./services/passport');
 const app = express();
+const authRoutes = require('./routes/authRoutes')
 
-
-app.get('/', (req,res) =>{
-    res.send({hi:'hello'});
-})
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT , ()=>{
+    console.log('server started at 5000');
+});
+
+// 4%2F0AfJohXkx1FxtQdu4LbSXUyCiqQIsz9CI0L27cGGjitlm78GmxetYckqZM8a5jHn5B_4alA
